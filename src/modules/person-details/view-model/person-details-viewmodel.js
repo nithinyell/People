5d5d3c2614735b6325d4edf6fbd81f9ce4b040ba
Nithin Yell <nithinyell@gmail.com>
2023-12-17 00:08:13 -0500

    Updated personal details

--- src/modules/person-details/view-model/person-details-viewmodel.js
+++ src/modules/person-details/view-model/person-details-viewmodel.js
@@ -3,21 +3,31 @@
 import { useEffect, useState } from "react";
 
 const PersonDetailsViewModel = (person) => {
-    const[personProfile, setPersonProfile] = useState([]);
+  const [personProfile, setPersonProfile] = useState([]);
+  const [editedPerson, setEditedPerson] = useState({...person});
 
-    useEffect(() => {
-        fetchPersonDetails(person)
-    }, [person]);
+  useEffect(() => {
+    fetchPersonDetails(person);
+  }, [person]);
 
-    const fetchPersonDetails = async(person) => {
-        console.log(person);
-        // In real time scenario we may fetch additional person detials by using personid from person object
-        // For now, just setting the details object as the person profile  
-        setPersonProfile(person);
-         
-    }
+  const handleEdit = (field, value) => {
+    setEditedPerson(prevPerson => ({
+      ...prevPerson,
+      [field]: value,
+    }));
+  };
 
-    return { personProfile };
+  const resetForm = () => {
+    setEditedPerson({...person});
+  };
+
+  const fetchPersonDetails = async person => {
+    // In real time scenario we may fetch additional person detials by using personid from person object
+    // For now, just setting the details object as the person profile
+    setPersonProfile(person);
+  };
+
+  return { personProfile, editedPerson, handleEdit, resetForm };
 };
 
 export { PersonDetailsViewModel };
--- src/modules/person-details/view/person-details.js
+++ src/modules/person-details/view/person-details.js
@@ -1,19 +1,79 @@
 // Person Details
 
-import { View,Text} from "react-native";
-import { PersonDetailsViewModel } from "../view-model/person-details-viewmodel";
+import {View, Text} from 'react-native';
+import {PersonDetailsViewModel} from '../view-model/person-details-viewmodel';
+import {useState} from 'react';
+import {Avatar, Card, Paragraph} from 'react-native-paper';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import { FlatList, TextInput } from 'react-native-gesture-handler';
 
-const PersonDetails = ({ route, navigation }) => {
-    const { person } = route.params;
-    const { personProfile } = PersonDetailsViewModel(person);
+const PersonDetails = ({route, navigation}) => {
+  const {person} = route.params;
+  const {personProfile, handleEdit, resetForm} = PersonDetailsViewModel(person);
+  const [isEditing, setIsEditing] = useState(false);
 
-    return (
-        <View>
-            <Text>
-                {personProfile.name}
-            </Text>
-        </View>
-    );
+  // Configure the header right button based on editing mode
+  navigation.setOptions({
+    headerRight: () => (
+      <HeaderButtons>
+        {isEditing ? (
+          <Item title="Save" iconName="save" onPress={saveChanges} />
+        ) : (
+          <Item title="Edit" iconName="pencil" onPress={toggleEditingMode} />
+        )}
+      </HeaderButtons>
+    ),
+  });
+
+  const saveChanges = () => {
+    // Perform API call or other save logic
+    console.log('Saving changes:', personProfile);
+    // After successful update, you might want to reset the form
+    resetForm();
+    setIsEditing(false);
+  };
+
+  const toggleEditingMode = () => {
+    setIsEditing(prev => !prev);
+  };
+
+  return (
+    <View style={{ alignItems: 'center', padding: 16 }}>
+      <Avatar.Text
+        size={100}
+        label={personProfile.name ? personProfile.name[0].toUpperCase() : ''}
+      />
+
+      {isEditing ? (
+        <TextInput
+          style={{ marginTop: 16, marginBottom: 16, padding: 8, borderWidth: 1, borderRadius: 4 }}
+          value={personProfile.name}
+          onChangeText={(text) => handleEdit('name', text)}
+        />
+      ) : (
+        <Text style={{ marginTop: 16, fontSize: 18, fontWeight: 'bold' }}>
+          {personProfile.name}
+        </Text>
+      )}
+
+      <Card style={{ width: '100%', marginTop: 16 }}>
+        <Card.Content>
+          <Paragraph>{personProfile.emailType}: {personProfile.email}</Paragraph>
+        </Card.Content>
+      </Card>
+
+      <Card style={{ width: '100%', marginTop: 16 }}>
+        <Card.Content>
+            <FlatList
+                data={personProfile.phoneNumbers}
+                renderItem={({item}) => <Paragraph>{item.type}: {item.number}</Paragraph> }
+                keyExtractor={item => item.number}
+            />
+        </Card.Content>
+      </Card>
+
+    </View>
+  );
 };
 
 export default PersonDetails;
