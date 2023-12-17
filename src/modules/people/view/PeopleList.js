dad3648b29d043ad5d02929274091224f667e1f1
Nithin Yell <nithinyell@gmail.com>
2023-12-17 00:32:19 -0500

    fix styling

--- src/modules/people/view/PeopleList.js
+++ src/modules/people/view/PeopleList.js
@@ -2,6 +2,7 @@
 
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { PeopleViewModel } from "../view-model/people-viewmodel";
+import { Avatar, Card } from "react-native-paper";
 
 const styles = StyleSheet.create({
     container: {
@@ -17,21 +18,43 @@ const styles = StyleSheet.create({
 const PeopleList = ({ navigation }) => {
     const { people } = PeopleViewModel(navigation);
     
-    const PersonItem = ({ person }) => {
-        return (
-            <View>
-                <TouchableOpacity onPress={() => { navigateToDetails(person) }}>
-                    <Text style={[styles.person]}>
-                        {person.name}
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            );
+    const PersonItem = ({person}) => {
+      return (
+        <TouchableOpacity onPress={() => navigateToDetails(person)}>
+          <View style={{padding: 5}}>
+            <Card
+              style={{
+                marginTop: 0,
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Card.Content
+                style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Avatar.Text
+                  size={25}
+                  label={person.name[0].toUpperCase()}
+                  style={{marginRight: 8, backgroundColor: getRandomColor()}}
+                />
+                <Text style={styles.person}>{person.name}</Text>
+              </Card.Content>
+            </Card>
+          </View>
+        </TouchableOpacity>
+      );
     };
 
     const navigateToDetails = (person) => {
         navigation.navigate("Person Details", { person })
     };
+
+    const getRandomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      };
 
     return (
         <View style={[styles.center]}>
